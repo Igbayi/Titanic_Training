85ad603acc383e13fcc3ffb5930e1f007c97feb2
Igbayi <adeolakhadija78@gmail.com>
2025-08-19 16:53:59 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 import streamlit as st
 import pickle
 
+st.title("titanic survival prediction")
 
-st.title("Salary prediction web Application")
-
-st.write("This web application will help to predict your salary based on your Gender and the year of experience you have worked")
+st.write("This web application will predict  the survival rate of passengers")
 
 
-model = pickle.load(open("final model.pkl",'rb'))
+model = pickle.load(open("Titanic_Model.pkl",'rb'))
 
-Yearofexperience = st.number_input("input you year of experience")
-Gender = st.number_input("input your Gender, Male -1 Female - 0")
+Pclass = st.number_input("input your passenger class, (first class= 1,second class = 2, third class = 3)")
+Age = st.number_input("what is the passengers age")
+SibSp = st.number_input("Numbers of sibling or spouse")
+Parch = st.number_input("Numbers of parent/children on board")
+Fare = st.number_input("Amount paid for the ticket")
+Sex_male = st.number_input("male = 1 , female = 0")
+Embarked_Q = st.number_input("For Q location , input 1 and if not input 0")
+Embarked_S = st.number_input("For S location , input 1 and if not input 0")
 
 if st.button('Predict'):
-    Pred  = model.predict([[Yearofexperience],[Gender]])
-    st.text(f'Your salry is {Pred[0]}')
-
+    Pred = model.predict([[Pclass, Age, SibSp, Parch, Fare, Sex_male, Embarked_Q, Embarked_S]])
+    st.text(f'Passengers survival rate is {Pred[0]}')
     st.text("Thank you for using the application")
+
+
+
+
+
+
+
+
+
+
