7ec980d302bda421ee37086fce08ec68b87ab255
Khadija <adeolakhadija78@gmail.com>
2025-08-19 16:26:01 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle
+import sklearn
+
+st.title("Salary prediction web Application")
+
+st.write("This web application will help to predict your salary based on your Gender and the year of experience you have worked")
+
+
+model = pickle.load(open("final model.pkl",'rb'))
+
+Yearofexperience = st.number_input("input you year of experience")
+Gender = st.number_input("input your Gender, Male -1 Female - 0")
+
+if st.button('Predict'):
+    Pred  = model.predict([[Yearofexperience],[Gender]])
+    st.text(f'Your salry is {Pred[0]}')
+
+    st.text("Thank you for using the application")
